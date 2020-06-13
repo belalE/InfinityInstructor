f0330fa1ce130d161026448fd04a71eb8a2294d6
belalE <balolyman1234@gmail.com>
2020-06-12 19:32:16 -0500

    Initial Commit

---
+++ InfinityInstructor/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  InfinityInstructor
+//
+//  Created by Belal Elsiesy on 6/12/20.
+//  Copyright © 2020 Belal Elsiesy. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
